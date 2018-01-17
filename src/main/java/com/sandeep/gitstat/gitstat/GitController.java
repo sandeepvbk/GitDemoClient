4edb0a8d40cd6a4478de5d915bfdd3d7a5aed2a2
Sandeep Kottur <sandeepkottur@Sandeeps-MBP.frontierlocal.net>
2018-01-17 11:48:08 -0600

    Working code to expose the git history service

---
+++ src/main/java/com/sandeep/gitstat/gitstat/GitController.java
@@ -0,0 +1,35 @@
+package com.sandeep.gitstat.gitstat;
+
+
+import javafx.application.Application;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.io.IOException;
+
+@RestController
+@RequestMapping(value="/rest/gitstatus")
+@CrossOrigin(origins = "http://localhost:8000")
+public class GitController {
+
+//    @RequestMapping(value = "/getAllRepo", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public String getGitRequest() throws IOException, ParseException {
+//       GitRestClient client = new GitRestClient();
+//
+//       return client.getAllRepo();
+//    }
+
+    @RequestMapping(value = "/getAllRepoLocal", method = RequestMethod.GET)
+    public String getGitRequestLocal() throws IOException, GitAPIException {
+
+        GitLocalRestClient localRestClient = new GitLocalRestClient();
+        return localRestClient.getAllRepo();
+
+    }
+}
